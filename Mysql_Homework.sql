5034095857800659d499c18e86a432def1955e94
Arin <arin_muk@hotmail.com>
2019-01-18 21:40:58 -0500

    homework step 7d completed

--- Mysql_Homework.sql
+++ Mysql_Homework.sql
@@ -18,4 +18,25 @@ show create table address;#--5a
 select s.first_name,s.last_name,a.address from staff s inner join address a on s.address_id=a.address_id; #-----6a
 select s.first_name,s.last_name,sum(p.amount) as "Total_amount" from staff s inner join payment p on s.staff_id=p.staff_id 
   where month(payment_date)=8 and year(payment_date)=2005 group by s.first_name,s.last_name;#-----6b
+select title, count(fa.actor_id) as number_of_actors from film f inner join film_actor fa on f.film_id=fa.film_id group by title;  #--6c
+select f.title,count(title) as "no_copies" from film f inner join inventory i on f.film_id=i.film_id 
+	where f.title="Hunchback Impossible"; #--6d
+select c.last_name, sum(p.amount) as "Total Amount Paid" from customer c inner join payment p on c.customer_id=p.customer_id 
+	group by c.last_name order by last_name ; ###-- 6e
+select title from film where title like "K%" or title like "Q%" and 
+language_id in
+(select distinct(l.language_id) from film f inner join language l on f.language_id=l.language_id where name = "English"); ##-- 7a
+
+select concat(ucase(a.first_name)," ",ucase(a.last_name)) as "Actor Name" from actor a inner Join film_actor fa on a.actor_id=fa.actor_id where fa.film_id in (
+select film_id from film where title = "Alone Trip"); #--7b
+
+select concat(ucase(cust.first_name)," ",ucase(cust.last_name)) as "Customer Name",email 
+ from customer cust inner join address a on cust.address_id=a.address_id where a.city_id in 
+(select ct.city_id from city ct join country c on c.country_id=ct.country_id where country ="Canada");#--7c
+
+    
+select title from film f inner join film_category fc on f.film_id=fc.film_id
+inner join category c on fc.category_id=c.category_id
+where name ="family";#--7d
+
 
